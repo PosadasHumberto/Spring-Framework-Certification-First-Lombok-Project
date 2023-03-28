92029949166d282a26fc49ab459b4971cc39931a
Humberto Posadas <posadas.humberto@outlook.com>
2023-03-28 15:47:57 +0200

    Creating project structure & POJO classes

---
+++ src/main/java/org/hposadas/projectlombok/model/Beer.java
@@ -0,0 +1,23 @@
+package org.hposadas.projectlombok.model;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Beer {
+
+    //atributos
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
