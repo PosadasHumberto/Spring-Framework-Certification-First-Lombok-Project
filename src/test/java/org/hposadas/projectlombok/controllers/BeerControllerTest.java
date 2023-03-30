f8aa0b7402b6f2863855c8066f03f8bf8fd65216
Humberto Posadas <posadas.humberto@outlook.com>
2023-03-30 23:11:06 +0200

    testing handlePost with MockMvc

--- src/test/java/org/hposadas/projectlombok/controllers/BeerControllerTest.java
+++ src/test/java/org/hposadas/projectlombok/controllers/BeerControllerTest.java
@@ -1,14 +1,21 @@
 package org.hposadas.projectlombok.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hposadas.projectlombok.model.Beer;
+import org.hposadas.projectlombok.model.BeerStyle;
 import org.hposadas.projectlombok.services.BeerService;
 import org.hposadas.projectlombok.services.BeerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.hamcrest.core.Is.*;
 import static org.mockito.BDDMockito.*;
@@ -24,12 +31,21 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean           //indica a mockito brindar un mock de BeerService al contexto Spring
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    BeerServiceImpl beerServiceImpl;
 
     //métodos
+
+    @BeforeEach
+    void setUp() {
+        beerServiceImpl = new BeerServiceImpl();
+    }
+
     @Test
     void listBeers() throws Exception {
         given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
@@ -56,4 +72,29 @@ class BeerControllerTest {
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 
+    @Test
+    void handlePost() throws Exception {
+
+        Beer beer = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Test Beer")
+                .beerStyle(BeerStyle.PILSNER)
+                .upc("898989")
+                .price(new BigDecimal(11))
+                .quantityOnHand(300)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beer);
+
+        mockMvc.perform(post("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().is(201))
+                .andExpect(header().exists("Location"));
+
+    }
 }
