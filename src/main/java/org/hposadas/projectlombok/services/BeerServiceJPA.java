03efe977a1e35bb9d3383c03a6c8610c43695e17
Humberto Posadas <posadas.humberto@outlook.com>
2023-04-05 15:33:34 +0200

    Definition JPA GET Operations

---
+++ src/main/java/org/hposadas/projectlombok/services/BeerServiceJPA.java
@@ -0,0 +1,69 @@
+package org.hposadas.projectlombok.services;
+
+import lombok.RequiredArgsConstructor;
+import org.hposadas.projectlombok.controllers.NotFoundException;
+import org.hposadas.projectlombok.mappers.BeerMapper;
+import org.hposadas.projectlombok.model.BeerDTO;
+import org.hposadas.projectlombok.repositories.BeerRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService{
+
+    //atributos
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    //métodos
+    @Override
+    public List<BeerDTO> listBeers() {
+        /**
+         * El método BeerRepository.findall() devuelve un List<Beer> pero como
+         * debemos devolver una List<BeerDTO> entonces convertimos esta lista a un
+         * Stream y mapeamos el elemento Beer en curso usando el método
+         * BeerMapper.beerToBeerDTO. Despues reduce el flujo y lo deja en forma de
+         * lista por lo que ahora tendremos una List<BeerDTO< que ha sido devuelta.
+         */
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+
+        return Optional.ofNullable(
+                beerMapper.beerToBeerDto(beerRepository.findById(id)
+                        .orElse(null))
+        );
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void ubdateBeerById(UUID id, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID id, BeerDTO beer) {
+
+    }
+}
---
+++ src/main/java/org/hposadas/projectlombok/services/CustomerserviceJPA.java
@@ -0,0 +1,58 @@
+package org.hposadas.projectlombok.services;
+
+import lombok.RequiredArgsConstructor;
+import org.hposadas.projectlombok.mappers.CustomerMapper;
+import org.hposadas.projectlombok.model.CustomerDTO;
+import org.hposadas.projectlombok.repositories.CustomerRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerserviceJPA implements Customerservice {
+
+    //atributos
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    //métodos
+    @Override
+    public List<CustomerDTO> getCustomers() {
+        /**
+         * El método CustomererRepository.findall() devuelve un List<Customer> pero como
+         * debemos devolver una List<CustomerDTO> entonces convertimos esta lista a un
+         * Stream y mapeamos el elemento Customer en curso usando el método
+         * CustomerMapper.customerToCustomerDTO. Despues reduce el flujo y lo deja en forma de
+         * lista por lo que ahora tendremos una List<CustomerDTO< que ha sido devuelta.
+         */
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO getCustomerById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID id, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID id) {
+
+    }
+}
