e914c8be14cd8a5616e902216a4320e3cb373ccb
Humberto Posadas <posadas.humberto@outlook.com>
2023-05-17 19:02:37 +0200

    Adding springDoc & Swagger Request Validator with tests

--- src/main/java/org/hposadas/projectlombok/config/SpringSecConfig.java
+++ src/main/java/org/hposadas/projectlombok/config/SpringSecConfig.java
@@ -2,15 +2,19 @@ package org.hposadas.projectlombok.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Profile("!test")
 public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
+                .requestMatchers("/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
         return http.build();
